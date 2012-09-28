31f5c127ca903b526342e7d14c15d3682164536a
Craig Radcliffe <chradcliffe@gmail.com>
2012-09-28 12:58:57 -0400

    Fix to check match object for null and to set a recurring alarm.

--- background.js
+++ background.js
@@ -4,6 +4,10 @@ function printError(e) {
             e, '').show();    
 }
 
+function setCheckAlarm() {
+    chrome.alarms.create({'periodInMinutes': 1, 'delayInMinutes': 1});
+}
+
 function checkMail() {
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
@@ -11,18 +15,19 @@ function checkMail() {
           return;
       }
       if (xhr.responseText) {
-          var count = xhr.responseText.match(/Message: Unread/g).length;
-          if (count > 0) {
-              chrome.alarms.clearAll();
-              var notification = webkitNotifications.createNotification(
-                  '', 'New mail message', 'You have ' + count + ' new message(s)'
-              );
-              notification.onclose = function() {
-                  chrome.alarms.create({'delayInMinutes': 1});
-              };
-              notification.show();
-          } else {
-              printError('Could not get message count from URL');              
+          var matches = xhr.responseText.match(/Message: Unread/g);
+          if (matches != null) {
+              var count = matches.length;
+              if (count > 0) {
+                  chrome.alarms.clearAll();
+                  var notification = webkitNotifications.createNotification(
+                      '', 'New mail message', 'You have ' + count + ' new message(s)'
+                  );
+                  notification.onclose = setCheckAlarm;
+                  notification.show();
+              } else {
+                  printError('Could not get message count from URL');
+              }
           }
       } else {
           printError('Fetching failed');
@@ -33,7 +38,7 @@ function checkMail() {
 }
 if (localStorage["url"] != ""){
     chrome.alarms.onAlarm.addListener(checkMail);
-    chrome.alarms.create({'delayInMinutes': 1});
+    setCheckAlarm();
     checkMail();
 } else {
     printError('owa-notification-chrome not configured');
