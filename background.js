783a760770e0b98f291ba2963feea91cf702ae83
Craig Radcliffe <chradcliffe@gmail.com>
2012-10-01 23:44:38 -0400

    Added tracking for already-closed notifications. You should only get a notification if you receive a notification for messages that are newer than the last closed notification.

--- background.js
+++ background.js
@@ -8,38 +8,58 @@ function setCheckAlarm() {
     chrome.alarms.create({'periodInMinutes': 1, 'delayInMinutes': 1});
 }
 
+function size(set) {
+    var size = 0;
+    for(var i in set) {
+        if (set.hasOwnProperty(i)) {
+            size++;
+        }
+    }
+    return size;
+}
+
+
+
 function checkMail() {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState != 4)  {
-          return;
-      }
-      if (xhr.responseText) {
-          var matches = xhr.responseText.match(/Message: Unread/g);
-          if (matches != null) {
-              var count = matches.length;
-              if (count > 0) {
-                  chrome.alarms.clearAll();
-                  var notification = webkitNotifications.createNotification(
-                      '', 'New mail message', 'You have ' + count + ' new message(s)'
-                  );
-                  notification.onclose = setCheckAlarm;
-                  notification.show();
-              } else {
-                  printError('Could not get message count from URL');
-              }
-          }
-      } else {
-          printError('Fetching failed');
-      }
-    };
-    xhr.open("GET", localStorage["url"], true);
-    xhr.send(null);
+    if (localStorage["message_set"] == undefined) {
+        localStorage["message_set"] = JSON.stringify(new Object());
+    }
+    $.get(localStorage["url"], function(data) {
+        var newSet = {};
+        var oldSet = JSON.parse(localStorage["message_set"]);
+        var shouldNotify = false;
+        $(data).find(".sI[alt='Message: Unread']").parent().siblings("td").find("input[name='chkmsg']")
+                .each(function() {
+            if (!(this.value in oldSet)) {
+                shouldNotify = true;
+            }
+            newSet[this.value] = null;
+        });
+        if (shouldNotify) {
+            var notification = webkitNotifications.createNotification('',
+                    'New mail message',
+                    'You have ' + size(newSet) + ' new message(s)');
+            notification.onclose = function() {
+                setCheckAlarm();
+                localStorage["message_set"] = JSON.stringify(newSet);
+            };
+            notification.show();
+            // TODO: fix this, it's not working for some reason
+            // chrome.tabs.query({url: localStorage["url"]+"/*"}, function(results) {
+            //     for (var tab in results) {
+            //         chrome.tabs.reload(tab.id);
+            //     }
+            // });
+        }
+    });
 }
-if (localStorage["url"] != ""){
-    chrome.alarms.onAlarm.addListener(checkMail);
-    setCheckAlarm();
-    checkMail();
-} else {
-    printError('owa-notification-chrome not configured');
-}
+
+$(document).ready(function() {
+    if (localStorage["url"] != ""){
+        chrome.alarms.onAlarm.addListener(checkMail);
+        setCheckAlarm();
+        checkMail();
+    } else {
+        printError('owa-notification-chrome not configured');
+    }
+});
